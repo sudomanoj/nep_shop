5653609c1c671271fbc94d62e7f630ddac46cf6e
sudomanoj <manoj.765421@bumc.tu.edu.np>
2025-01-16 22:34:12 +0545

    update urls.py

--- src/nep_shop/urls.py
+++ src/nep_shop/urls.py
@@ -25,22 +25,24 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
+site_urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('auth_profile.urls')),
     path('product/', include('product.urls')),
 ]
 
-urlpatterns += [
+jwt_urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += [
+swagger_urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns = site_urlpatterns + jwt_urlpatterns + swagger_urlpatterns
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
